812a5544e6d154fb2315c7f6559e5da593febbe0
Safak Tandogan <safaktandogan1@gmail.com>
2017-07-10 16:02:17 +0300

    tabs edited

--- app/src/main/java/com/example/safak/starwars/StarWarsMain.java
+++ app/src/main/java/com/example/safak/starwars/StarWarsMain.java
@@ -115,11 +115,23 @@ public class StarWarsMain extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_star_wars_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView2 = inflater.inflate(R.layout.fragment_star_wars_main, container, false);
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER))
+            {
+                case 1:
+                    TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                    textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+                    return rootView;
+                case 2:
+                    return rootView2;
+            }
+
+
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
         }
     }
