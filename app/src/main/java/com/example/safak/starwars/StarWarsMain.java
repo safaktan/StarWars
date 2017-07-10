160cab223368d579ad294c2d4097f417232abf77
Koray Demirok <kry.demirok@gmail.com>
2017-07-10 15:55:44 +0300

    TABS

--- app/src/main/java/com/example/safak/starwars/StarWarsMain.java
+++ app/src/main/java/com/example/safak/starwars/StarWarsMain.java
@@ -143,18 +143,17 @@ public class StarWarsMain extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "PEOPLE";
                 case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
+                    return "PLANETS";
+
             }
             return null;
         }
