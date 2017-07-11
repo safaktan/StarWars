0dd0ea22755dad2bcb1913712ea99164e439251f
Koray Demirok <kry.demirok@gmail.com>
2017-07-11 16:37:22 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/safak/starwars/StarWarsMain.java

---
+++ app/src/main/java/com/example/safak/starwars/PlanetsAdapter.java
@@ -0,0 +1,53 @@
+package com.example.safak.starwars;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Koray on 11.07.2017.
+ */
+
+public class PlanetsAdapter extends RecyclerView.Adapter<PlanetsAdapter.ViewHolder>  {
+
+    private List<PlanetsInfo> planetsList;
+
+    public PlanetsAdapter(List<PlanetsInfo> planetsList) {
+        this.planetsList = planetsList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_Item, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        holder.nameText.setText(planetsList.get(position).getName());
+
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.nameText.setText(planetsList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return planetsList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameText;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            nameText=(TextView)itemView.findViewById(R.id.person_name);
+        }
+    }
+}
