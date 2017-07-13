3a41600cb27adc8eb9644736a7cec178d998e92d
Safak Tandogan <safaktandogan1@gmail.com>
2017-07-13 14:38:06 +0300

    People Side is done

--- app/src/main/java/com/example/safak/starwars/StarWarsMain.java
+++ app/src/main/java/com/example/safak/starwars/StarWarsMain.java
@@ -4,6 +4,8 @@ import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -20,80 +22,44 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import com.example.safak.starwars.pojos.PeopleInfo;
 import com.example.safak.starwars.pojos.PeopleInfoEvent;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.util.List;
+
 public class StarWarsMain extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_wars_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(StarWarsMain.this);
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(StarWarsMain.this);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private static final String ARG_SECTION_NUMBER = "section_number";
+        public RecyclerView peopleRV;
+        public List<PeopleInfo> peopleList;
+        public PeopleAdapter peopleAdapter;
+
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -103,13 +69,16 @@ public class StarWarsMain extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_star_wars_main, container, false);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.people_page, container, false);
             View rootView2 = inflater.inflate(R.layout.planet_page, container, false);
+            peopleRV = (RecyclerView) rootView.findViewById(R.id.peopleRV);
 
-            switch (getArguments().getInt(ARG_SECTION_NUMBER))
-            {
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
                 case 1:
+                    RetrofitClass retrofitClass=new RetrofitClass();
+                    retrofitClass.getPeopleRequest();
+
                     return rootView;
                 case 2:
                     return rootView2;
@@ -119,22 +88,29 @@ public class StarWarsMain extends AppCompatActivity {
         }
 
         @Subscribe
-        public void getEvent(PeopleInfoEvent event)
-        {
-
-
-
+        public void getEventPeople(PeopleInfoEvent event) {
+            Log.d("MyApp", "getEventPeople: " + event.getPeopleInfoEventList().size());
+            peopleAdapter=new PeopleAdapter(event.getPeopleInfoEventList());
+            peopleRV.setLayoutManager(new LinearLayoutManager(getContext()));
+            peopleRV.setAdapter(peopleAdapter);
         }
 
 
+        @Override
+        public void onStart() {
+            super.onStart();
+            EventBus.getDefault().register(this);
+        }
 
 
+        @Override
+        public void onStop() {
+            super.onStop();
+            EventBus.getDefault().unregister(this);
+        }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -143,14 +119,11 @@ public class StarWarsMain extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return 2;
         }
 
